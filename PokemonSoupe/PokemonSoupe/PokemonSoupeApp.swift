d6cf2b92e0e7c8b6f0f7ff2ed2d2c6cd057e7818
InnaChystiakova <inna_islyamova@ukr.net>
2024-05-08 19:19:49 +0100

    Apply .gitignore

---
+++ PokemonSoupe/PokemonSoupe/PokemonSoupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSoupeApp.swift
+//  PokemonSoupe
+//
+//  Created by Inna Chystiakova on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSoupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
